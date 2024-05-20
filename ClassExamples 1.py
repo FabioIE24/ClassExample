60846647c8e1e09f9b78b3cb02fba21099712af5
FabioIE24 <156927851+FabioIE24@users.noreply.github.com>
2024-05-20 14:04:44 +0200

    ClassExamples 1.py

---
+++ ClassExamples 1.py
@@ -0,0 +1,61 @@
+class Point:
+    def __init__(self, x, y):
+        """
+        Init method that initializes the point with x and y
+        :param x: X coordinate value
+        :param y: Y coordinate value
+        """
+        self.x = x
+        self.y = y
+        # In my own instance of x and y, I am storing the values of x and y
+
+    def __str__(self):
+        """
+        How to print this point?
+        :return:
+        """
+        return f"({self.x},{self.y})"
+
+    def __repr__(self):
+        """
+                How to print this point if in a list?
+                :return:
+                """
+        return self.__str__()
+    @property
+    def distfromorigin(self):
+        distsquared = (self.x)**2 + (self.y)**2
+        dist = distsquared**0.5
+        return dist
+    def __gt__(self, other):
+        my_size = self.distfromorigin()
+        their_size = other.distfromorigin()
+        return my_size > their_size
+    def __eq__(self,other):
+        """
+        How to compare with ==?
+        :param other: the other point instance
+        :return:
+        """
+        return self.distfromorigin() == other.distfromorigin()
+
+
+if __name__ == "__main__":
+    # add random point
+    import random
+    france = []
+    for i in range(0, 5):
+        france.append(Point(random.randint(1,99), random.randint(1,99)))
+
+    print(france) # iterates and calls point.__repr__
+
+    for point in france:
+        print(point.distfromorigin())
+
+    a = Point(2, 3)
+    b = Point(7, 9)
+    print(a.distfromorigin()>b.distfromorigin())
+    print(a>b)
+
+    france.sort()
+    print(f"Thy point with greatest distance from origin is: {france[-1]} and thou smallest point is: {france[0]}")
